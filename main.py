fb30acfd4ec8c0c837ab3cb4dc776d51bbd9da88
jjanmo <jjanmo07@hanmail.net>
2021-04-25 19:46:43 +0900

    Get totals of countries of confirmed, deaths, recovered using groupby

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 daily_df = pd.read_csv('data/daily_report.csv')
+
+# get totals data of confirmed, deaths, recovered
 totals_df = daily_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
 totals_df = totals_df.rename(columns={'index': 'condition'})
+
+# get totals of countries of confirmed, deaths, recovered
+countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
